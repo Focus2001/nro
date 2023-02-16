acbf8e3b18fd51d242640691d4c387f577e034d3
Focus2001 <tranvanphu14112001@gmail.com>
2023-02-16 19:09:46 +0700

    js

---
+++ mainjs/main.js
@@ -0,0 +1,8 @@
+var fullName = "Tran Van Phu";
+alert(fullName);
+console.log("log");
+confirm("xac nhan ban du tuoi");
+prompt("ban bao nhieu tuoi");
+setInterval(function () {
+  console.log("oki");
+}, 1000);
